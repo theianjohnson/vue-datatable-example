848d5566a314bc654e4bd3c117006abb48c90bcc
Ian Johnson <ian@ianjohnson.net>
2018-08-15 09:46:40 -0700

    Initial

---
+++ src/components/Datatable.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot></slot>
+    <table border="1">
+      <tr>
+        <td>Table cell 1</td>
+        <td>Table cell 2</td>
+        <td>Table cell 3</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    queryData() {
+      // eslint-disable-next-line
+      console.log('Querying data from server (calling api endpoint, etc)');
+    }
+  }
+}
+</script>
---
+++ src/components/DateFilter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <a @click.prevent="$emit('dates-selected', {start: '2018-07-15', end: '2018-08-15'})" href="#">Select 2018-07-15 to 2018-08-15</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
